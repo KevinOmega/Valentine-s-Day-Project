54085506683b6bfc6b73618504a835ab606b64ca
Kevin Huayllas <KevinOmega01@gmail.com>
2024-02-14 13:31:25 -0400

    initial commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { home_image } from '../api'
+
+const Home = () => {
+  return (
+    <div id='home_section' style={{backgroundImage :`url(${require("../images/home.jpg")})`}}>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ src/api.js
@@ -0,0 +1,4 @@
+
+const home_image= "/public/images/home.jpg"
+
+export {home_image};
